f32b9cfc72051baf1182ca90569678387766e267
cal-brown <calvinlongbrown@gmail.com>
2020-11-06 04:04:55 +0000

    added print optional to sequence scoring and calculated conservation score for each 33mer

--- sequence_scoring.py
+++ sequence_scoring.py
@@ -1,20 +1,23 @@
 import sys
 
-def input_aligned_sequences(filename):
+def input_aligned_sequences(filename, print_on=False):
     sequence = ""
     sequences = []
     sequence_name_mode = -1
     with open(filename) as fileobj:
-        print("SEQUENCES FOUND")
+        if print_on:
+            print("SEQUENCES FOUND")
         for line in fileobj:
             if line[:1] == ">":
-                print(line[1:-1])
+                if print_on:
+                    print(line[1:-1])
                 if sequence != "":
                     sequences.append(sequence)
                 sequence = ""
             else:
                 sequence += line[:-1]
-    print("")
+    if print_on:
+        print("")
     return sequences
 
 def check_aligned_sequences(sequences):
@@ -27,7 +30,7 @@ def main():
     if len(sys.argv) != 2 and len(sys.argv) != 3:
         raise ValueError("Incorrect command line argument usage. Correct: python sequence_scoring.py [filename]")
     print("\n" + "---------------[" + sys.argv[1] + "]---------------" + "\n")
-    sequences = input_aligned_sequences(sys.argv[1])
+    sequences = input_aligned_sequences(sys.argv[1], print_on=True)
     if len(sys.argv) == 3:
         if sys.argv[2] == "-A":
             check_aligned_sequences(sequences)
